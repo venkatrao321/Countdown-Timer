1d85ffbf86bb12aeae66b71248098bd9e437da5b
venkatrao321 <venkatrao321@gmail.com>
2024-03-21 16:57:59 +0530

    added Validations

--- countdownTimer/src/App.jsx
+++ countdownTimer/src/App.jsx
@@ -1,23 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
+function currentDateTime() {
+  var currentDateTime = new Date();
+  var year = currentDateTime.getFullYear();
+  var month = (currentDateTime.getMonth() + 1).toString().padStart(2, "0"); // Month starts from 0
+  var day = currentDateTime.getDate().toString().padStart(2, "0");
+  var hours = currentDateTime.getHours().toString().padStart(2, "0");
+  var minutes = currentDateTime.getMinutes().toString().padStart(2, "0");
+  // Format the date and time
+  var formattedDateTime =
+    year + "-" + month + "-" + day + "T" + hours + ":" + minutes;
+  return formattedDateTime;
+}
+function diffInDays(selectedTime) {
+  const date1 = new Date(currentDateTime());
+  const date2 = new Date(selectedTime);
+  const diffTime = Math.abs(date2 - date1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays;
+}
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
+  const [selectedDate, setSelectedDate] = useState("");
+  const onChangeDateTime = (e) => {
+    console.log(e.target.value);
+    if (diffInDays(e.target.value) > 100) {
+      alert("Selected time is more than 100days");
+      setSelectedDate("");
+    } else setSelectedDate(e.target.value);
+  };
 
   return (
     <>
-     <input
-  type="datetime-local"
-  id="meeting-time"
-  name="meeting-time"
-  value=""
-  min="2024-06-07T00:00"
-  max="2025-06-14T00:00" />
+      <label className="inputboxselect">Countdown Timer</label>
+      <input
+        className="inputboxselect"
+        type="datetime-local"
+        id="meeting-time"
+        name="meeting-time"
+        value={selectedDate}
+        min={currentDateTime()}
+        onChange={onChangeDateTime}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
